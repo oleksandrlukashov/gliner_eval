abe210ec03056d075296b3fd5f56a0dc45cf26ed
oleksandrlukashov <alexandrlukashow@gmail.com>
2024-12-04 20:21:02 +0200

    fixed path

--- classes/re_classes/re_crossre.py
+++ classes/re_classes/re_crossre.py
@@ -1,6 +1,9 @@
+import sys
+import os
 from datasets import load_dataset
 from tqdm import tqdm
 from gliner import GLiNER
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from classes.parent_class import AbstractEvaluator
 import re
 
